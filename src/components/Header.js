334423350a5f037e3a7c43dfa76b86daab997fb8
KlientKonnect <info@klientkonnect.com>
2025-04-22 20:05:29 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#0f172a', padding: '1rem', color: '#fff' }}>
+      <h2>🔗 Klient Konnect CRM</h2>
+    </header>
+  );
+}
+
+export default Header;

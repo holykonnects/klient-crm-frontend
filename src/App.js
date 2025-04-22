bbf3cbe35b3b359d1910ca4c804df3e63148661c
KlientKonnect <info@klientkonnect.com>
2025-04-22 15:32:34 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to Klient Konnect CRM</h1>
+      <p>This is your custom-built frontend using React + Google Sheets.</p>
+    </div>
+  );
+}
+
+export default App;

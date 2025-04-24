411bc9aa2a80c030f0555c3e04a155821b200e75
KlientKonnect <info@klientkonnect.com>
2025-04-24 19:47:19 +0530

    Create AddLead.js

---
+++ src/pages/AddLead.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function AddLead() {
+  return <Typography variant="h4">Add New Lead (Form Integration Coming)</Typography>;
+}
+
+export default AddLead;

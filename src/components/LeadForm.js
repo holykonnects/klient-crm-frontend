3bee8a64488452479b2efa5437c25121da168a41
KlientKonnect <info@klientkonnect.com>
2025-04-25 14:44:23 +0530

    Update LeadForm.js

--- src/components/LeadForm.js
+++ src/components/LeadForm.js
@@ -32,97 +32,104 @@ function LeadForm() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Paper elevation={3} sx={{ maxWidth: 900, margin: '2rem auto', padding: 4 }}>
-        <Typography variant="h5" fontWeight="bold" color="#6495ED" mb={3}>
-          Add New Lead
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            {[
-              ['Lead Owner', 'leadOwner'],
-              ['First Name', 'firstName'],
-              ['Last Name', 'lastName'],
-              ['Company', 'company'],
-              ['Mobile', 'mobile'],
-              ['Email', 'email'],
-              ['Fax', 'fax'],
-              ['Website', 'website'],
-              ['Lead Source', 'leadSource'],
-              ['Industry', 'industry'],
-              ['No. of Employees', 'employees'],
-              ['Annual Revenue', 'revenue'],
-              ['Social Media', 'social'],
-              ['Street', 'street'],
-              ['City', 'city'],
-              ['State', 'state'],
-              ['Country', 'country'],
-              ['Pincode', 'pincode']
-            ].map(([label, name]) => (
-              <Grid item xs={12} sm={6} key={name}>
+      <Box sx={{ maxWidth: 1000, margin: '2rem auto' }}>
+        {/* Logo + Title Header */}
+        <Box display="flex" alignItems="center" justifyContent="space-between" mb={2} px={2}>
+          <img src="/assets/kk-logo.png" alt="Klient Konnect" style={{ height: 80 }} />
+          <Typography variant="h5" fontWeight="bold" color="#6495ED">
+            Add New Lead
+          </Typography>
+        </Box>
+
+        <Paper elevation={3} sx={{ padding: 4 }}>
+          <Box component="form" onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              {[
+                ['Lead Owner', 'leadOwner'],
+                ['First Name', 'firstName'],
+                ['Last Name', 'lastName'],
+                ['Company', 'company'],
+                ['Mobile', 'mobile'],
+                ['Email', 'email'],
+                ['Fax', 'fax'],
+                ['Website', 'website'],
+                ['Lead Source', 'leadSource'],
+                ['Industry', 'industry'],
+                ['No. of Employees', 'employees'],
+                ['Annual Revenue', 'revenue'],
+                ['Social Media', 'social'],
+                ['Street', 'street'],
+                ['City', 'city'],
+                ['State', 'state'],
+                ['Country', 'country'],
+                ['Pincode', 'pincode']
+              ].map(([label, name]) => (
+                <Grid item xs={12} sm={6} key={name}>
+                  <TextField
+                    fullWidth
+                    label={label}
+                    name={name}
+                    value={lead[name]}
+                    onChange={handleChange}
+                    size="small"
+                  />
+                </Grid>
+              ))}
+
+              {/* Lead Status Dropdown */}
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  select
+                  fullWidth
+                  label="Lead Status"
+                  name="leadStatus"
+                  value={lead.leadStatus}
+                  onChange={handleChange}
+                  size="small"
+                >
+                  {['New', 'Contacted', 'Qualified'].map(option => (
+                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+
+              {/* Description TextArea */}
+              <Grid item xs={12}>
                 <TextField
                   fullWidth
-                  label={label}
-                  name={name}
-                  value={lead[name]}
+                  multiline
+                  minRows={2}
+                  label="Description"
+                  name="description"
+                  value={lead.description}
                   onChange={handleChange}
                   size="small"
                 />
               </Grid>
-            ))}
 
-            {/* Lead Status Dropdown */}
-            <Grid item xs={12} sm={6}>
-              <TextField
-                select
-                fullWidth
-                label="Lead Status"
-                name="leadStatus"
-                value={lead.leadStatus}
-                onChange={handleChange}
-                size="small"
-              >
-                {['New', 'Contacted', 'Qualified'].map(option => (
-                  <MenuItem key={option} value={option}>{option}</MenuItem>
-                ))}
-              </TextField>
+              {/* Additional Description */}
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  multiline
+                  minRows={2}
+                  label="Additional Description"
+                  name="additionalDescription"
+                  value={lead.additionalDescription}
+                  onChange={handleChange}
+                  size="small"
+                />
+              </Grid>
             </Grid>
 
-            {/* Description TextArea */}
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                multiline
-                minRows={2}
-                label="Description"
-                name="description"
-                value={lead.description}
-                onChange={handleChange}
-                size="small"
-              />
-            </Grid>
-
-            {/* Additional Description */}
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                multiline
-                minRows={2}
-                label="Additional Description"
-                name="additionalDescription"
-                value={lead.additionalDescription}
-                onChange={handleChange}
-                size="small"
-              />
-            </Grid>
-          </Grid>
-
-          <Box mt={3} display="flex" justifyContent="flex-end">
-            <Button type="submit" variant="contained" sx={{ backgroundColor: '#6495ED' }}>
-              Submit Lead
-            </Button>
+            <Box mt={3} display="flex" justifyContent="flex-end">
+              <Button type="submit" variant="contained" sx={{ backgroundColor: '#6495ED' }}>
+                Submit Lead
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </Box>
     </ThemeProvider>
   );
 }

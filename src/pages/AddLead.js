b81daaef988162f6ff821f98ba656d0b37f35133
KlientKonnect <info@klientkonnect.com>
2025-04-25 13:44:34 +0530

    Delete src/pages/AddLead.js

--- src/pages/AddLead.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Typography } from '@mui/material';
-
-function AddLead() {
-  return <Typography variant="h4">Add New Lead (Form Integration Coming)</Typography>;
-}
-
-export default AddLead;

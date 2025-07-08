dbde1a54932324115ef881a5c0a1bca89b82e7ec
KlientKonnect <info@klientkonnect.com>
2025-07-08 14:38:20 +0530

    Update AccountsTable.js

--- src/components/AccountsTable.js
+++ src/components/AccountsTable.js
@@ -42,26 +42,26 @@ function AccountsTable() {
   const validationUrl = 'https://script.google.com/macros/s/AKfycbyaSwpMpH0RCTQkgwzme0N5WYgNP9aERhQs7mQCFX3CvBBFARne_jsM5YW6L705TdET/exec';
   const submitUrl = 'https://script.google.com/macros/s/AKfycbxZ87qfE6u-2jT8xgSlYJu5dG6WduY0lG4LmlXSOk2EGkWBH4CbZIwEJxEHI-Bmduoh/exec';
 
- useEffect(() => {
-  fetch(dataUrl)
-    .then(res => res.json())
-    .then(data => {
-      let filteredDeals = data;
+   useEffect(() => {
+    fetch(dataUrl)
+      .then(res => res.json())
+      .then(data => {
+        let filteredAccounts = data;
 
-      if (role === 'End User') {
-        filteredDeals = data.filter(deal =>
-          deal['Account Owner'] === username || deal['Lead Owner'] === username || deal['Owner'] === username
-        );
-      }
+        if (role === 'End User') {
+          filteredAccounts = data.filter(acc =>
+            acc['Account Owner'] === username || acc['Lead Owner'] === username || acc['Owner'] === username
+          );
+        }
 
-      setDeals(filteredDeals);
-      setVisibleColumns(filteredDeals.length ? Object.keys(filteredDeals[0]) : []);
-      setLoading(false);
-    });
+        setAccounts(filteredAccounts);
+        setVisibleColumns(filteredAccounts.length ? Object.keys(filteredAccounts[0]) : []);
+        setLoading(false);
+      });
 
-  fetch(validationUrl)
-    .then(res => res.json())
-    .then(setValidationData);
+    fetch(validationUrl)
+      .then(res => res.json())
+      .then(setValidationData);
   }, [username, role]); // re-run if user/role changes
 
 

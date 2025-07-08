8920900e45cbcd4a5d833e2aadf9d1994b6b8339
KlientKonnect <info@klientkonnect.com>
2025-07-08 09:17:40 +0530

    Update LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -22,11 +22,11 @@ function LoginPage() {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch('https://script.google.com/macros/s/AKfycbzl-2rhvZEeVj3vvV1tLvv1zJlOQ6xxDlttVXOePHwJ0A_0JCp3a_TWIC-dpPE_2g3wZA/exec', {
+      const res = await fetch('/api/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
-      });
+        body: JSON.stringify({ email, password })
+    });
 
       const result = await res.json();
       console.log('Login result:', result);

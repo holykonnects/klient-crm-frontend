70174144029db2ccf59f657bc14e11fc54ba2a1f
KlientKonnect <info@klientkonnect.com>
2025-07-11 16:49:45 +0530

    Update ManageTender.js

--- src/components/ManageTender.js
+++ src/components/ManageTender.js
@@ -33,7 +33,7 @@ const ManageTender = () => {
   const fetchValidationOptions = async () => {
     const res = await fetch('https://script.google.com/macros/s/AKfycbyaSwpMpH0RCTQkgwzme0N5WYgNP9aERhQs7mQCFX3CvBBFARne_jsM5YW6L705TdET/exec');
     const data = await res.json();
-    setValidationOptions(data['Tender Validation Table'] || {});
+    setValidationOptions(data['Tender Validation Tables'] || {});
   };
 
   const handleChange = (key, value) => {

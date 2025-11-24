79a5534617f36f21e7f420e65b5549873d482d94
KlientKonnect <info@klientkonnect.com>
2025-11-24 13:18:40 +0530

    Update LeadFormModalWrapper.jsx

--- src/components/email/LeadFormModalWrapper.jsx
+++ src/components/email/LeadFormModalWrapper.jsx
@@ -1,17 +1,28 @@
 import { Dialog, Box } from "@mui/material";
-
-// ⭐ Correct path to LeadForm.jsx
 import LeadForm from "../LeadForm";
 
-export default function LeadFormModalWrapper({ open, onClose, onLeadCreated }) {
+export default function LeadFormModalWrapper({
+  open,
+  onClose,
+  onLeadCreated
+}) {
+  const handleSuccess = (lead) => {
+    // 1. Immediately close the modal BEFORE LeadForm resets itself
+    onClose();
+
+    // 2. Push the lead back to SendEmailModal
+    onLeadCreated(lead);
+
+    // 3. DO NOT LET LeadForm reopen itself
+    // (we do NOT call any reset or allow ref mounts)
+  };
+
   return (
     <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
       <Box sx={{ p: 2 }}>
         <LeadForm
           mode="add"
-          onSuccess={(lead) => {
-            onLeadCreated(lead);
-          }}
+          onSuccess={handleSuccess}
           onCancel={onClose}
         />
       </Box>

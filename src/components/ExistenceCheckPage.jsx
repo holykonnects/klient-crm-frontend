3272e711f23a1cb6e1571c68ff41af1ba21bc544
KlientKonnect <info@klientkonnect.com>
2026-01-13 17:08:37 +0530

    Update ExistenceCheckPage.jsx

--- src/components/ExistenceCheckPage.jsx
+++ src/components/ExistenceCheckPage.jsx
@@ -1,11 +1,9 @@
+// ExistenceCheckPage.jsx
 import React from "react";
 import { Box, Typography, Paper } from "@mui/material";
 import ExistenceSearch from "./ExistenceSearch";
 
 const cornflowerBlue = "#6495ED";
-
-const EXISTENCE_SEARCH_BACKEND_URL =
-  "https://script.google.com/macros/s/AKfycbzlQehb3L1sGPfJY0llUf-24bu8PjiGAJtEGXvh6mQJkbfDJLTU2o4g_HLxinBX_q6B4g/exec";
 
 export default function ExistenceCheckPage() {
   return (
@@ -13,7 +11,7 @@ export default function ExistenceCheckPage() {
       <Typography
         sx={{
           fontSize: 20,
-          fontWeight: 800,
+          fontWeight: 900,
           color: cornflowerBlue,
           mb: 2,
           fontFamily: "Montserrat, sans-serif"
@@ -31,8 +29,7 @@ export default function ExistenceCheckPage() {
           background: "#fff"
         }}
       >
-        {/* Full-width, comfortable UX */}
-        <ExistenceSearch backendUrl={EXISTENCE_SEARCH_BACKEND_URL} open={true} />
+        <ExistenceSearch />
       </Paper>
     </Box>
   );

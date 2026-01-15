76ebb59ad1af82a61a0b2c742c3acd4f25e0512a
KlientKonnect <info@klientkonnect.com>
2026-01-15 09:16:41 +0530

    Update DealsTable.js

--- src/components/DealsTable.js
+++ src/components/DealsTable.js
@@ -130,7 +130,6 @@ function DealsTable() {
       .then((res) => res.json())
       .then(setValidationData)
       .catch((e) => console.error("Validation fetch error:", e));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [username, role]);
 
   const handleSort = (key) => {
